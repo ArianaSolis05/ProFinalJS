2202028ae2b0a2f9c1866400ecf52e5f5b4acc68
LuisZeledon21 <lzeledon.fwd21@gmail.com>
2025-08-27 16:03:03 -0600

    js-login-registro

--- public/js/login.js
+++ public/js/login.js
@@ -1,26 +1,20 @@
+import { getData } from "../services/fetch.js";
 
-const email = document.getElementById("email")
-const password = document.getElementById("password")
-const btnIngresar = document.getElementById("btnIngresar")
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const btnIngresar = document.getElementById("btnIngresar");
 
-let users = []
+btnIngresar.addEventListener("click", async function () {
+  let users = await getData("usuarios");
+  const usuarioEncontrado = users.find(
+    (user) =>
+      user.correoUsuario === email.value && user.claveUsuario === password.value
+  );
+  console.log(usuarioEncontrado);
 
-btnIngresar.addEventListener("click", function () {
-
-    const usuarios = {
-        email: email.value,
-        password: password.value,
-    }
-    console.log(usuarios);
-
-    if (email === "" && password === "") {
-        return true
-    }else{
-        alert("usuario incorrecto")
-    }
-    
+  if (!usuarioEncontrado) {
+    alert("NO QUELE");
+    return;
+  }
+  alert("SI QUELE");
 });
-
-
-
-
