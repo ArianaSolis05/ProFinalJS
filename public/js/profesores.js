deef14004262816dd5e67816611fc4907c8be2af
ArianaSolis05 <arianasolisfwd@gmail.com>
2025-08-29 15:42:05 -0600

    funcion

---
+++ public/js/profesores.js
@@ -0,0 +1,28 @@
+import { getData } from "../services/fetch.js"
+
+const listaConsultas = document.getElementById("listaConsultas")
+
+
+async function traerConsultas() {
+    const consultas = await getData("ticket")
+    consultas.forEach((consulta)=>{
+    const nombreCompleto = document.createElement("p")
+    const correo = document.createElement("p")
+    const tipoConsulta = document.createElement("p")
+    const mensaje = document.createElement("p")
+
+    nombreCompleto.textContent = consulta.nombreCompleto
+    correo.textContent = consulta.correoElectronico
+    tipoConsulta.textContent = consulta.tipoConsulta
+    mensaje.textContent = consulta.mensaje
+
+    listaConsultas.setAttribute("class", "lista");
+
+
+    listaConsultas.appendChild(nombreCompleto)
+    listaConsultas.appendChild(correo)
+    listaConsultas.appendChild(tipoConsulta)
+    listaConsultas.appendChild(mensaje)
+    })
+}
+traerConsultas()
